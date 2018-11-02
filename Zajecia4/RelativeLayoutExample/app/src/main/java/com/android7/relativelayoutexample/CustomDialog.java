2ae6d70a956c856cb1fccc2d67aaf25aaaec5dd7
PrzemyslawStok <przemyslaw.stoklosa@gmail.com>
2018-11-02 15:46:14 +0100

    dialog example

---
+++ Zajecia4/RelativeLayoutExample/app/src/main/java/com/android7/relativelayoutexample/CustomDialog.java
@@ -0,0 +1,53 @@
+package com.android7.relativelayoutexample;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+public class CustomDialog extends DialogFragment {
+    public interface OnCustomDialogListener{
+        void setText(String text);
+    }
+
+    OnCustomDialogListener mListener;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        View view = inflater.inflate(R.layout.custom_dialog,null);
+        final EditText editText = view.findViewById(R.id.edit_text);
+
+
+        builder.setMessage(getString(R.string.dialog_2_message))
+                .setPositiveButton(getString(R.string.dialog_positive_button), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if(mListener!=null){
+                            mListener.setText(editText.getText().toString());
+                        }
+                    }
+                })
+                .setNegativeButton(getString(R.string.dialog_negative_button), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+
+        builder.setView(view);
+        // Create the AlertDialog object and return it
+        return builder.create();
+
+    }
+
+    public void setOnCustomClickListener(OnCustomDialogListener listener){
+        mListener = listener;
+    }
+}
---
+++ Zajecia4/RelativeLayoutExample/app/src/main/java/com/android7/relativelayoutexample/MainActivity.java
@@ -0,0 +1,86 @@
+package com.android7.relativelayoutexample;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button0;
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage(getString(R.string.dialog_1_message));
+                builder.setPositiveButton(getString(R.string.dialog_positive_button), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builder.setNegativeButton(getString(R.string.dialog_negative_button), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CustomDialog customDialog = new CustomDialog();
+                customDialog.show(getSupportFragmentManager(),"Custom dialog");
+
+                customDialog.setOnCustomClickListener(new CustomDialog.OnCustomDialogListener() {
+                    @Override
+                    public void setText(String text) {
+                        Toast.makeText(MainActivity.this,text,Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menu_dialog_1:
+                Toast.makeText(this,getString(R.string.open_dilog_1),Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_dialog_2:
+                Toast.makeText(this,getString(R.string.open_dilog_2),Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
