aadfbdd97ccfe22b79cfdc5b28699b1fb0412dc5
PrzemyslawStok <przemyslaw.stoklosa@gmail.com>
2018-11-17 11:31:06 +0100

    cd ..

--- Zajecia5/ConstraintLayoutExampleA/app/src/main/java/com/android7/constrainlayoutexample/MainActivityFragment.java
+++ Zajecia5/ConstraintLayoutExampleA/app/src/main/java/com/android7/constrainlayoutexample/MainActivityFragment.java
@@ -1,15 +1,23 @@
 package com.android7.constrainlayoutexample;
 
-import android.support.v4.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    ImageView image;
+
+    Button button0;
+    Button button1;
 
     public MainActivityFragment() {
     }
@@ -17,6 +25,29 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        image = view.findViewById(R.id.imageView0);
+
+        button0 = view.findViewById(R.id.button0);
+        button1 = view.findViewById(R.id.button1);
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                image.setBackgroundColor(Color.YELLOW);
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                image.setBackgroundColor(Color.GREEN);
+            }
+        });
+
+
+        return view;
     }
 }
