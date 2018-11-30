96b2e5af025e5170f87b64c615b72144f818b12d
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2018-11-30 13:28:06 +0100

    Graph and database example added

---
+++ Zajecia8/MyApplication/app/src/main/java/com/android7/plotanddatabaseexample/APIInterface.java
@@ -0,0 +1,11 @@
+package com.android7.plotanddatabaseexample;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+interface APIInterface {
+
+    @GET("/servtmp?")
+    Call<Temperature> getWSIZTemperature(@Query("limit") int no);
+}
---
+++ Zajecia8/MyApplication/app/src/main/java/com/android7/plotanddatabaseexample/Temperature.java
@@ -0,0 +1,13 @@
+package com.android7.plotanddatabaseexample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Temperature {
+
+    public List<DataTemp> dataTemp = new ArrayList();
+
+    public class DataTemp{
+
+    }
+}
