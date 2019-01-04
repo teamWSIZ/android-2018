7eed2cda29adf9daa2f7e41db2161040d1d54823
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-01-04 16:20:41 +0100

    cd..

--- Zajecia12/PlotAndDatabaseExampleB/app/src/main/java/com/android7/plotanddatabaseexampleb/MainActivityFragment.java
+++ Zajecia12/PlotAndDatabaseExampleB/app/src/main/java/com/android7/plotanddatabaseexampleb/MainActivityFragment.java
@@ -37,27 +37,62 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-        mGraphView_1 = (GraphView) view.findViewById(R.id.graph_1);
-        mGraphView_2 = (GraphView) view.findViewById(R.id.graph_2);
+        mGraphView_1 = view.findViewById(R.id.graph_1);
+        mGraphView_2 = view.findViewById(R.id.graph_2);
 
         apiInterface = APIClient.getClient().create(APIInterface.class);
 
-        int limit = 1000;
+        int limit = 100;
 
-        mGraphView_1.addSeries(getTemperatureData(Color.BLUE,limit,1,1));
-        mGraphView_1.addSeries(getTemperatureData(Color.RED,limit,2,2));
-        mGraphView_1.addSeries(getTemperatureData(getResources().getColor(R.color.plotGreen),limit,1,3));
-        mGraphView_1.addSeries(getTemperatureData(getResources().getColor(R.color.plotOrange),limit,2,4));
+        //mGraphView_1.addSeries(getTemperatureData(Color.BLUE,limit,1,1));
+        //mGraphView_1.addSeries(getTemperatureData(Color.RED,limit,2,2));
+        mGraphView_1.addSeries(getTemperatureData(Color.BLUE,limit,3,true));
+        mGraphView_1.addSeries(getTemperatureData(Color.RED,limit,4,true));
+        //mGraphView_1.addSeries(getTemperatureData(getResources().getColor(R.color.plotGreen),limit,3,true));
+        //mGraphView_1.addSeries(getTemperatureData(getResources().getColor(R.color.plotOrange),limit,4,true));
 
-        mGraphView_2.addSeries(getHumidityData(Color.BLUE,limit,1,1));
-        mGraphView_2.addSeries(getHumidityData(Color.RED,limit,2,1));
-        mGraphView_2.addSeries(getHumidityData(getResources().getColor(R.color.plotGreen),limit,1,3));
-        mGraphView_2.addSeries(getHumidityData(getResources().getColor(R.color.plotOrange),limit,2,4));
+        mGraphView_2.addSeries(getTemperatureData(Color.BLUE,limit,1,true));
+        mGraphView_2.addSeries(getHumidityData(Color.RED,limit,1,true));
+
+        //mGraphView_2.addSeries(getHumidityData(Color.BLUE,limit,1,1));
+        //mGraphView_2.addSeries(getHumidityData(Color.RED,limit,2,1));
+        //mGraphView_2.addSeries(getHumidityData(getResources().getColor(R.color.plotGreen),limit,1,3));
+        //mGraphView_2.addSeries(getHumidityData(getResources().getColor(R.color.plotOrange),limit,2,4));
 
         return view;
     }
 
-    public LineGraphSeries<DataPoint> getTemperatureData(int color, int limit, int sensor, final double mul){
+    private Double getMaxTemperature(List<Temperature> temperatures){
+
+        if(temperatures.size()<=0)
+            return null;
+
+        double maxTemp = 1;
+
+        for(Temperature temp0:temperatures){
+            if(temp0.temperature>maxTemp)
+                maxTemp = temp0.temperature;
+        }
+
+        return maxTemp;
+    }
+
+    private Double getMaxHumidity(List<Humidity> humidities){
+
+        if(humidities.size()<=0)
+            return null;
+
+        double maxHumidity = 1;
+
+        for(Humidity humidity:humidities){
+            if(humidity.humidity>maxHumidity)
+                maxHumidity = humidity.humidity;
+        }
+
+        return maxHumidity;
+    }
+
+    public LineGraphSeries<DataPoint> getTemperatureData(int color, int limit, int sensor, final boolean normData){
         Call<List<Temperature>> call = apiInterface.getTemperature(limit,sensor);
 
         final LineGraphSeries<DataPoint> temperature = new LineGraphSeries<DataPoint>();
@@ -68,8 +103,13 @@ public class MainActivityFragment extends Fragment {
             public void onResponse(Call<List<Temperature>> call, Response<List<Temperature>> response) {
                 List<Temperature> data = response.body();
 
+                double maxTemperature = 1.0;
+
+                if(normData)
+                    maxTemperature = getMaxTemperature(data);
+
                 for(int i=0;i<data.size();i++)
-                    temperature.appendData(new DataPoint(i++,data.get(i).temperature*mul),false,data.size());
+                    temperature.appendData(new DataPoint(i++,data.get(i).temperature/maxTemperature),false,data.size());
 
             }
 
@@ -84,7 +124,7 @@ public class MainActivityFragment extends Fragment {
         return temperature;
     }
 
-    public LineGraphSeries<DataPoint> getHumidityData(int color, int limit, int sensor, final double mul){
+    public LineGraphSeries<DataPoint> getHumidityData(int color, int limit, int sensor, final boolean normData){
         Call<List<Humidity>> call = apiInterface.getHumidity(limit,sensor);
 
         final LineGraphSeries<DataPoint> temperature = new LineGraphSeries<DataPoint>();
@@ -95,8 +135,13 @@ public class MainActivityFragment extends Fragment {
             public void onResponse(Call<List<Humidity>> call, Response<List<Humidity>> response) {
                 List<Humidity> data = response.body();
 
+                double maxHumidity= 1.0;
+
+                if(normData)
+                    maxHumidity = getMaxHumidity(data);
+
                 for(int i=0;i<data.size();i++)
-                    temperature.appendData(new DataPoint(i++,data.get(i).humidity*mul),false,data.size());
+                    temperature.appendData(new DataPoint(i++,data.get(i).humidity/maxHumidity),false,data.size());
 
             }
 
