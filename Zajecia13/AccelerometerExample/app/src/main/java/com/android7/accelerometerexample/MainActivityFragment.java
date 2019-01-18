06224a94a1b20495cbba8434b2eff3a2ff1c774b
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-01-18 18:53:17 +0100

    cd..

--- Zajecia13/AccelerometerExample/app/src/main/java/com/android7/accelerometerexample/MainActivityFragment.java
+++ Zajecia13/AccelerometerExample/app/src/main/java/com/android7/accelerometerexample/MainActivityFragment.java
@@ -7,6 +7,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,15 +37,40 @@ public class MainActivityFragment extends Fragment implements SensorEventListene
 
         mSensorManager = (SensorManager)getActivity().getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        mSensorManager.registerListener(this,mAccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor sensor = sensorEvent.sensor;
+
+        if(sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            double x = sensorEvent.values[0];
+            double y = sensorEvent.values[1];
+            double z = sensorEvent.values[2];
+
+            Log.d("Akcelerometr","x:"+x+", y:"+y+", z:"+z);
+        }
 
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mSensorManager.registerListener(this,mAccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
