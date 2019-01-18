9d39dfc81405c6b448806f069c1bc8789ffd69b8
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-01-18 18:21:11 +0100

    cd..

--- Zajecia13/AccelerometerExample/app/src/main/java/com/android7/accelerometerexample/MainActivityFragment.java
+++ Zajecia13/AccelerometerExample/app/src/main/java/com/android7/accelerometerexample/MainActivityFragment.java
@@ -1,7 +1,12 @@
 package com.android7.accelerometerexample;
 
-import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,14 +14,37 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements SensorEventListener {
+
+    SensorManager mSensorManager;
+    Sensor mAccelerometer;
 
     public MainActivityFragment() {
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        mSensorManager = (SensorManager)getActivity().getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
