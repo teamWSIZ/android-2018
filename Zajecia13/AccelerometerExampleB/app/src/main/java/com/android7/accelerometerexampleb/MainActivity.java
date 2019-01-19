d73a2360f1596859b0c8d272d99d0cfa9cb4bd19
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-01-19 10:35:35 +0100

    cd..

--- Zajecia13/AccelerometerExampleB/app/src/main/java/com/android7/accelerometerexampleb/MainActivity.java
+++ Zajecia13/AccelerometerExampleB/app/src/main/java/com/android7/accelerometerexampleb/MainActivity.java
@@ -1,13 +1,51 @@
 package com.android7.accelerometerexampleb;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager mSensorManager;
+    Sensor mAccelerometer;
+
+    SensorEventListener mAcceleremeterListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            Log.d("Sensor","Accelerometer listener 2");
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        mSensorManager.registerListener(this,mAccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(mAcceleremeterListener,mAccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        Log.d("Sensor","accelerometer");
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
