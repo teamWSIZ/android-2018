fb76d04d0fbb36f0be00cffce7fcb4394df5c045
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-01-19 09:43:20 +0100

    cd..

--- Zajecia13/AccelerometerExampleA/app/src/main/java/com/android7/accelerometerexamplea/MainActivity.java
+++ Zajecia13/AccelerometerExampleA/app/src/main/java/com/android7/accelerometerexamplea/MainActivity.java
@@ -8,16 +8,26 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     SensorManager mSensorManager;
     Sensor mAccelerometer;
 
+    TextView mAxTextView;
+    TextView mAyTextView;
+    TextView mAzTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        mAxTextView = findViewById(R.id.ax_value);
+        mAyTextView = findViewById(R.id.ay_value);
+        mAzTextView = findViewById(R.id.az_value);
 
         mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -35,7 +45,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             double Ay = sensorEvent.values[1];
             double Az = sensorEvent.values[2];
 
-            Log.i("Accelerometer", "Ax: "+Ax+", Ay: "+Ay+", Az: "+Az);
+            mAxTextView.setText("Ax:"+Ax);
+            mAyTextView.setText("Ay:"+Ay);
+            mAzTextView.setText("Az:"+Az);
+
+            double a = Math.sqrt(Ax*Ax+Ay*Ay+Az*Az);
+
+            Log.i("Accelerometer", "Przyspieszenie:"+a);
 
         }
     }
@@ -43,5 +59,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mSensorManager.registerListener(this,mAccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
