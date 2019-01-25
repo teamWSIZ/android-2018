1adafe0fb953204c4ac7250cf1865766950be7fb
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-01-25 18:53:18 +0100

    cd..

--- Zajecia14/OrientationExample/app/src/main/java/com/android7/myapplication/CompassView.java
+++ Zajecia14/OrientationExample/app/src/main/java/com/android7/myapplication/CompassView.java
@@ -9,23 +9,38 @@ import android.view.View;
 public class CompassView extends View {
 
     Paint mPaint;
+    float mAzimuth = 0;
 
     public CompassView(Context context) {
         super(context);
 
         mPaint = new Paint();
         mPaint.setColor(Color.BLUE);
-        mPaint.setStrokeWidth(1);
+        mPaint.setStrokeWidth(5);
         mPaint.setAntiAlias(true);
 
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        float width = getMeasuredWidth();
-        float height = getMeasuredHeight();
+        int xcenter = getMeasuredWidth() / 2;
+        int ycenter = getMeasuredHeight() / 2;
 
-        canvas.drawRect(0,0,width,height,mPaint);
+        float radius = (float) (Math.max(xcenter, ycenter) * 0.7);
+
+        mPaint.setColor(Color.WHITE);
+        canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint);
+
+        mPaint.setColor(Color.GRAY);
+        canvas.drawCircle(xcenter, ycenter, radius, mPaint);
+
+        mPaint.setColor(Color.BLUE);
+
+        canvas.drawLine(
+                xcenter,
+                ycenter,
+                xcenter + radius * (float)Math.sin(-mAzimuth / 180 * Math.PI),
+                (ycenter - radius * (float)Math.cos(-mAzimuth / 180 * Math.PI)), mPaint);
 
     }
 }
