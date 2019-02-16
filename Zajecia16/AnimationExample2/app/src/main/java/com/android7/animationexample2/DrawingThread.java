1d8587a172126913906a7a955fd27892dd09c177
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-02-16 10:47:23 +0100

    cd..

--- Zajecia16/AnimationExample2/app/src/main/java/com/android7/animationexample2/DrawingThread.java
+++ Zajecia16/AnimationExample2/app/src/main/java/com/android7/animationexample2/DrawingThread.java
@@ -1,10 +1,38 @@
 package com.android7.animationexample2;
 
 import android.util.Log;
+import android.widget.TextView;
 
 public class DrawingThread extends Thread {
+
+    private boolean isWorking;
+    private int number;
+
+    TextView mTextView;
+
+    DrawingThread(TextView textView){
+        mTextView = textView;
+
+        mTextView.setText("Number: "+number);
+
+        isWorking = true;
+        number = 0;
+
+    }
+
+
     @Override
     public void run() {
-        Log.i("thread","thread");
+
+        while(isWorking) {
+            Log.i("thread", "Number: "+number);
+            number++;
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- Zajecia16/AnimationExample2/app/src/main/java/com/android7/animationexample2/MainActivity.java
+++ Zajecia16/AnimationExample2/app/src/main/java/com/android7/animationexample2/MainActivity.java
@@ -1,7 +1,8 @@
 package com.android7.animationexample2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DrawingThread thread = new DrawingThread();
+        DrawingThread thread = new DrawingThread((TextView)findViewById(R.id.textView));
         thread.start();
+
+
     }
 }
